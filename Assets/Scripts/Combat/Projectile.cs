10e6a41b6c8632efbc9b5d9569d0d9410378d2d2
Vincent Beilke <vincent.beilke@gmail.com>
2023-04-21 12:12:31 -0500

    Added Projectiles and cleaned up folder structure

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -8,17 +8,24 @@ namespace RPGProject.Assets.Scripts.Combat
     public class Projectile : MonoBehaviour
     {        
         [SerializeField] private float _speed = 1f;
+        [SerializeField] private bool _isHoming = false;
+        [SerializeField] private GameObject _hitEffect = null;
 
         private Health _target = null;
         public Health Target { set { _target = value; } }
 
         private float _damage = 0f;
 
+        private void Start()
+        {
+            transform.LookAt(GetAimLocation());
+        }
+
         private void Update()
         {
             if (_target == null) return;
 
-            transform.LookAt(GetAimLocation());
+            if (_isHoming && !_target.IsDead) transform.LookAt(GetAimLocation());
             transform.Translate(Vector3.forward * _speed* Time.deltaTime);
         }
 
@@ -43,7 +50,16 @@ namespace RPGProject.Assets.Scripts.Combat
         private void OnTriggerEnter(Collider other)
         {
             if (other.GetComponent<Health>() != _target) return;
+            
+            if (_target.IsDead) return;
+
             _target.TakeDamage(_damage);
+
+            if (_hitEffect != null)
+            {
+                Instantiate(_hitEffect, GetAimLocation(), transform.rotation);
+            }
+
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,4 +1,5 @@
 using RPGProject.Assets.Scripts.Core;
+using System;
 using UnityEngine;
 
 namespace RPGProject.Assets.Scripts.Combat
@@ -12,6 +13,8 @@ namespace RPGProject.Assets.Scripts.Combat
         [SerializeField] private bool _isRightHanded = true;
         [SerializeField] private Projectile _projectile = null;
 
+        const string WeaponName = "Weapon";
+
         public float Range { get { return _range; } }
         public float AttackSpeed { get { return _attackSpeed; } }
         public float Damage { get { return _damage; } }
@@ -21,11 +24,14 @@ namespace RPGProject.Assets.Scripts.Combat
 
         public void Spawn(Transform rightHand, Transform leftHand, Animator animator)
         {
+            DestroyOldWeapon(rightHand, leftHand);
+
             if (_equippedPrefab != null)
             {
                 Transform handTransform = GetHandTransform(rightHand, leftHand);
 
-                Instantiate(_equippedPrefab, handTransform);
+                var weapon = Instantiate(_equippedPrefab, handTransform);
+                weapon.name = WeaponName;
             }
 
             if (_animatorOverride != null)
@@ -50,6 +56,16 @@ namespace RPGProject.Assets.Scripts.Combat
         private Transform GetHandTransform(Transform rightHand, Transform leftHand)
         {
             return _isRightHanded ? rightHand.transform : leftHand.transform;
+        }
+
+        private void DestroyOldWeapon(Transform rightHand, Transform leftHand)
+        {
+            var oldWeapon = rightHand.Find(WeaponName);
+            if (oldWeapon == null) oldWeapon = leftHand.Find(WeaponName);
+            if (oldWeapon == null) return;
+
+            oldWeapon.name = "DESTROY";
+            Destroy(oldWeapon.gameObject);
         }
     }
 }
